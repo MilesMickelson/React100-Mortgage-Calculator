bb7ff0580419b62986b44d961ec9b20b6c84e930
Miles Mickelson <milesmickelson87@gmail.com>
2020-01-17 14:41:35 -0800

    refactors code, leaving minimally styled from projects completion commit

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -31,39 +31,34 @@ export default class App extends React.Component {
     });
   }
 
-  render() {
-    return (
+	render() {
+		return (
       <div className='container'>
-      <h3>Mortgage Calculator</h3>
-        <div className="row">
-          <label>Loan Balance</label>
-          <input name="balance" type="number" placeholder="0" value={this.state.balance} onChange={this.handleChange}/>
-        </div>
-        <div className="row">
-        <label>Annual Percentage Rate</label>
-          <input name="rate" type="number" placeholder="0" step="0.01" value={this.state.rate} onChange={this.handleChange}/>
-        </div>
-        <div className="row">
-          <label>Loan Term</label>
-          <select name="term" value={this.state.term} onChange={this.handleChange}>
-            <option>Choose term</option>
-            <option name="Fifteen" value="15">15 year</option>
-            <option name="Thirty" value="30">30 year</option>
-          </select>
-        </div>
-        <div className="row">
-          <button name="submit" onClick={this.handleCalculation}>Submit</button>
-        </div>
-        <div className="row">
-          <div id="output" value={this.state.text}>{this.state.text}</div>
-        </div>
-      </div>
+				<h1>Mortgage Calculator</h1>
+				<hr></hr>
+				<div className='row'>
+					<label>Loan Balance</label>
+					<input name='balance' type='number' placeholder='0' value={this.state.balance} onChange={this.handleChange}/>
+				</div>
+				<div className='row'>
+				<label>Annual Percentage Rate</label>
+					<input name='rate' type='number' placeholder='0' step='0.01' value={ this.state.rate } onChange={this.handleChange}/>
+				</div>
+				<div className='row'>
+					<label>Loan Term</label>
+					<select name='term' value={this.state.term} onChange={this.handleChange}>
+						<option>Choose term</option>
+						<option name='Fifteen' value='15'>15 year</option>
+						<option name='Thirty' value='30'>30 year</option>
+					</select>
+				</div>
+				<div className='row'>
+					<button name='submit' onClick={this.handleCalculation}>Submit</button>
+				</div>
+				<div className='row'>
+					<div id='output' value={this.state.text}>{this.state.text}</div>
+				</div>
+			</div>
     );
   }
 }
-
-// ReactDOM.render(
-//   <default />,
-//   document.getElementById('root')
-// );
-
